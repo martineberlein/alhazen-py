f8de4d2a4230eff98fcb37f15945de03a52f7b42
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-02-16 22:52:24 +0100

    separate learner classes

--- src/alhazen/learner.py
+++ src/alhazen/learner.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from typing import List
+from abc import ABC, abstractmethod
 
 from sklearn.tree import DecisionTreeClassifier
 from pandas import DataFrame
@@ -26,3 +28,48 @@ def train_tree(data: DataFrame) -> DecisionTreeClassifier:
     # self.__tree = treetools.remove_infeasible(clf, features) # MARTIN: This is optional, but is a nice extension
     # that results in nicer decision trees
     return clf
+
+
+class Learner(ABC):
+    def __init__(self):
+        self.learner = None
+
+    @abstractmethod
+    def learn(self, data: List, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_input_specifications(self):
+        raise NotImplementedError
+
+
+class DecisionTreeLearner(Learner):
+    def __init__(
+        self,
+        min_sample_leaf: int = 1,
+        min_samples_split: int = 2,
+        max_features=None,
+        max_depth: int = 5,
+    ):
+        super().__init__()
+        self.min_sample_leaf = min_sample_leaf
+        self.min_sample_split = min_samples_split
+        self.max_features = max_features
+        self.max_depth = max_depth
+
+    def learn(self, data: List, **kwargs):
+        raise NotImplementedError
+
+    def get_input_specifications(self):
+        raise NotImplementedError
+
+
+class RandomForestLearner(Learner):
+    def __init__(self):
+        super().__init__()
+
+    def learn(self, **kwargs):
+        raise NotImplementedError
+
+    def get_input_specifications(self):
+        raise NotImplementedError
